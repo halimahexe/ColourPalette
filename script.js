a7bc5717fda43a73d71429631c150d51081b6fbb
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-12-28 18:05:31 +0000

    fixing and making all colours automatically generate

--- script.js
+++ script.js
@@ -12,16 +12,25 @@ const colourTwo = document.querySelector("#colour-two");
 const colourThree = document.querySelector("#colour-three");
 const colourFour = document.querySelector("#colour-four");
 const colourFive = document.querySelector("#colour-five");
+const textOne = document.querySelector("#text-one");
+const textTwo = document.querySelector("#text-two");
+const textThree = document.querySelector("#text-three");
+const textFour = document.querySelector("#text-four");
+const textFive = document.querySelector("#text-five");
 
-function randCol(boxColour) {
-    let digitOne = Math.floor(Math.random() * colours.one.length);
-    let digitTwo = Math.floor(Math.random() * colours.two.length);
-    let digitThree = Math.floor(Math.random() * colours.three.length);
-    let digitFour = Math.floor(Math.random() * colours.four.length);
-    let digitFive = Math.floor(Math.random() * colours.five.length);
-    let digitSix = Math.floor(Math.random() * colours.six.length);
+function randCol(boxColour, boxText) {
+    let digitOne = colours.one[Math.floor(Math.random() * colours.one.length)];
+    let digitTwo = colours.two[Math.floor(Math.random() * colours.two.length)];
+    let digitThree = colours.three[Math.floor(Math.random() * colours.three.length)];
+    let digitFour = colours.four[Math.floor(Math.random() * colours.four.length)];
+    let digitFive = colours.five[Math.floor(Math.random() * colours.five.length)];
+    let digitSix = colours.six[Math.floor(Math.random() * colours.six.length)];
     let hexCode = `#${digitOne}${digitTwo}${digitThree}${digitFour}${digitFive}${digitSix}`
     boxColour.style.backgroundColor = hexCode;
+    boxText.innerText = hexCode;
 }
-
-randCol(colourTwo);
+randCol(colourOne, textOne);
+randCol(colourTwo, textTwo);
+randCol(colourThree, textThree);
+randCol(colourFour, textFour);
+randCol(colourFive, textFive);
