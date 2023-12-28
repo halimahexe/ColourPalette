ca3b8621c449cd27af291ed9fee6c76a49620db4
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-12-28 17:45:21 +0000

    basic html structure

---
+++ script.js
@@ -0,0 +1,5 @@
+const colourOne = document.querySelector("#colour-one");
+const colourTwo = document.querySelector("#colour-two");
+const colourThree = document.querySelector("#colour-three");
+const colourFour = document.querySelector("#colour-four");
+const colourFive = document.querySelector("#colour-five");
