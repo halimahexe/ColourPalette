9f6d9469be4fb9c2d1979fe925490a9317efa56c
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-12-29 14:58:37 +0000

    simplifying randCol function

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const colours = {
-    one: ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    two: ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    three: ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    four: ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    five: ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    six: ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-}
+const colours = 'abcdef0123456789';
 
 const colourOne = document.querySelector("#colour-one");
 const colourTwo = document.querySelector("#colour-two");
@@ -19,13 +12,12 @@ const textFour = document.querySelector("#text-four");
 const textFive = document.querySelector("#text-five");
 
 function randCol(boxColour, boxText) {
-    let digitOne = colours.one[Math.floor(Math.random() * colours.one.length)];
-    let digitTwo = colours.two[Math.floor(Math.random() * colours.two.length)];
-    let digitThree = colours.three[Math.floor(Math.random() * colours.three.length)];
-    let digitFour = colours.four[Math.floor(Math.random() * colours.four.length)];
-    let digitFive = colours.five[Math.floor(Math.random() * colours.five.length)];
-    let digitSix = colours.six[Math.floor(Math.random() * colours.six.length)];
-    let hexCode = `#${digitOne}${digitTwo}${digitThree}${digitFour}${digitFive}${digitSix}`
+    let hexCode = '#';
+
+    for (let i = 0; i < 6; i++) {
+        hexCode += colours[Math.floor(Math.random() * colours.length)];
+    }
+
     boxColour.style.backgroundColor = hexCode;
     boxText.innerText = hexCode;
 }
